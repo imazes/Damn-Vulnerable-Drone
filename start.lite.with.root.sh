8d9a847e42f6d1e5744c31c3ab5264a26814d35c
root <root@damn.lan>
2026-01-21 09:30:45 +0000

    ...

--- start.lite.with.root.sh
+++ start.lite.with.root.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -x
+# set -x
+cd "$(dirname "$0")"
+
 export DISPLAY=:0
 sudo -u damn env DISPLAY=:0 xhost +SI:localuser:root
 
 CC_SVC="companion-computer-lite"
 GCS_SVC="ground-control-station-lite"
 SIM_SVC="simulator-lite"
+./stop.sh
 docker compose -f docker-compose-lite.yaml up -d
-docker compose -f docker-compose-lite.yaml logs -f "$SIM_SVC" "$CC_SVC" "$GCS_SVC"
+docker compose -f docker-compose-lite.yaml logs -f "$SIM_SVC" "$CC_SVC" "$GCS_SVC" 2>&1 |tee dvd.log
