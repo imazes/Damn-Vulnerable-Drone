08436aec430bb131471c48d4a0047e54bcad3506
Your Name <you@example.com>
2026-01-21 10:04:00 +0000

    repair display

--- start.lite.with.root.sh
+++ start.lite.with.root.sh
@@ -4,10 +4,12 @@ cd "$(dirname "$0")"
 
 export DISPLAY=:0
 sudo -u damn env DISPLAY=:0 xhost +SI:localuser:root
+sudo -u damn env DISPLAY=:0 xhost +local:docker 
 
 CC_SVC="companion-computer-lite"
 GCS_SVC="ground-control-station-lite"
 SIM_SVC="simulator-lite"
 ./stop.sh
+# ./start.sh --no-wifi
 docker compose -f docker-compose-lite.yaml up -d
 docker compose -f docker-compose-lite.yaml logs -f "$SIM_SVC" "$CC_SVC" "$GCS_SVC" 2>&1 |tee dvd.log
