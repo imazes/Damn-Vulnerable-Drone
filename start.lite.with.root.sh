f2035cbebb04c01364f752039c4d2c378beeb7d0
Your Name <you@example.com>
2026-01-22 07:10:12 +0000

    change opt dir

--- start.lite.with.root.sh
+++ start.lite.with.root.sh
@@ -6,12 +6,17 @@ export DISPLAY=:0
 sudo -u damn env DISPLAY=:0 xhost +SI:localuser:root
 sudo -u damn env DISPLAY=:0 xhost +local:docker 
 
+# 镜像名称
 CC_SVC="companion-computer-lite"
 GCS_SVC="ground-control-station-lite"
 SIM_SVC="simulator-lite"
+
 ./stop.sh
 # ./start.sh --no-wifi
-docker compose -f docker-compose-lite.yaml build
+# 刷新构建,删除旧的构建
+docker compose -f docker-compose-lite.yaml build && docker image prune -f
+# 后台启动项目
 docker compose -f docker-compose-lite.yaml up -d
+# 记录日志
 docker compose -f docker-compose-lite.yaml logs -f "$SIM_SVC" "$CC_SVC" "$GCS_SVC" 2>&1 > dvd.log &
 tail -f dvd.log
